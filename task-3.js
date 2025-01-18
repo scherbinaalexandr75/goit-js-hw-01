adc1c09d164450423c979e025472252cb16d6ee1
Shcherbyna Oleksandr <Scherbinaalexandr75@gmail.com>
2025-01-18 16:55:25 +0200

    Summary

---
+++ task-3.js
@@ -0,0 +1,11 @@
+function getElementWidth(content, padding, border) {
+  const valueCont = Number.parseInt(content);
+  const valuePadd = Number.parseInt(padding);
+  const valueBord = Number.parseInt(border);
+  const value = valueCont + valuePadd * 2 + valueBord * 2;
+  return value;
+}
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
