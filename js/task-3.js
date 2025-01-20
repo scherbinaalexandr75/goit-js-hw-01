a9e3f5b6b2f85ddb8c46d2b558c566137c5b575f
Shcherbyna Oleksandr <Scherbinaalexandr75@gmail.com>
2025-01-20 18:32:11 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,8 @@
 function getElementWidth(content, padding, border) {
-  const valueContent = Number.parseInt(content);
-  const valuePadding = Number.parseInt(padding);
-  const valueBorder = Number.parseFloat(border);
-  const value = valueContent + valuePadding * 2 + valueBorder * 2;
-  return value;
+  content = Number.parseInt(content);
+  padding = Number.parseInt(padding);
+  border = Number.parseFloat(border);
+  return content + padding * 2 + border * 2;
 }
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
